f6162b31f30177ec0c02e3ec3025e0744b3504a8
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2023-06-01 16:38:51 -0400

    log fixes

--- src/funkvis/LogHelper.hx
+++ src/funkvis/LogHelper.hx
@@ -6,4 +6,10 @@ class LogHelper
     {
         return Math.log(x) / Math.log(2);
     }
+
+
+    public static function log10(x:Float):Float
+    {
+        return Math.log(x) / Math.log(10);
+    };
 }
